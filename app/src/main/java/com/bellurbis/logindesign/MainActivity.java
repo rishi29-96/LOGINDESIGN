5a7118f324e2230f08d395da2b320297ea866915
rishi29-96 <69304648+rishi29-96@users.noreply.github.com>
2020-09-23 18:11:16 +0530

    DESIGNING A LOGIN SCREEN WITH MUSIC

--- app/src/main/java/com/bellurbis/logindesign/MainActivity.java
+++ app/src/main/java/com/bellurbis/logindesign/MainActivity.java
@@ -2,13 +2,18 @@ package com.bellurbis.logindesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.Settings;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer oursong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MediaPlayer oursong = MediaPlayer.create(MainActivity.this, R.raw.music);
+        oursong.start();
     }
 }
